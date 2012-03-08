534d4f1d070f977cff8a572b4cb15563c2b416ac
Carl Hörberg <carl.hoerberg@gmail.com>
2012-03-08 17:44:57 +0100

    inline comments

--- app.rb
+++ app.rb
@@ -3,9 +3,9 @@ require 'sinatra/streaming'
 require 'haml'
 require 'amqp'
 
-enable :loggning
 configure do
   EM.next_tick do
+    # Connect to CloudAMQP and set the default connection
     AMQP.connection = AMQP.connect ENV['CLOUDAMQP_URL'] || 'amqp://guest:guest@localhost'
   end
 end
@@ -15,6 +15,7 @@ get '/' do
 end
 
 post '/publish' do
+  # publish a message to a fanout exchange
   AMQP::Channel.new do |channel|
     channel.fanout("f1").publish "Hello, world!"
   end
@@ -25,13 +26,17 @@ get '/stream', provides: 'text/event-stream' do
   stream :keep_open do |out|
     AMQP::Channel.new do |channel|
       channel.queue do |queue|
+        # create a queue and bind it to the fanout exchange
         queue.bind(channel.fanout("f1")).subscribe do |payload|
           out << "data: #{payload}\n\n"
         end
       end
+
+      # add a timer to keep the connection alive 
       timer = EM.add_periodic_timer(20) { out << ":\n" } 
+      
+      # clean up when the user closes the stream
       out.callback do
-        p 'closing'
         timer.cancel
         channel.close
       end
