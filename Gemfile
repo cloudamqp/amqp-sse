4cbec82fc7dd75e4325693534401d50482a8e4a4
Carl Hörberg <carl.hoerberg@gmail.com>
2012-03-08 16:53:34 +0100

    initial commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source :rubygems
+
+gem 'thin'
+gem 'sinatra'
+gem 'sinatra-contrib'
+gem 'haml'
+gem 'amqp'
---
+++ app.rb
@@ -0,0 +1,39 @@
+require 'sinatra'
+require 'sinatra/streaming'
+require 'haml'
+require 'amqp'
+
+configure do
+  EM.next_tick do
+    AMQP.connect ENV['CLOUDAMQP_URL'] || 'amqp://guest:guest@localhost'
+  end
+end
+
+get '/' do
+  haml :index
+end
+
+post '/publish' do
+  AMQP::Channel.new do |channel|
+    channel.fanout("f1").publish "Hello, world!"
+  end
+  204
+end
+
+get '/stream', provides: 'text/event-stream' do
+  stream :keep_open do |out|
+    AMQP::Channel.new do |channel|
+      channel.queue do |queue|
+        queue.bind(channel.fanout("f1")).subscribe do |payload|
+          out << "data: #{payload}\n\n"
+        end
+      end
+      timer = EM.add_periodic_timer(20) { out << ":\n" } 
+      out.callback do
+        p 'closing'
+        timer.cancel
+        channel.close
+      end
+    end
+  end
+end
